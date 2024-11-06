acc706e0b4345fb1a90494656641a5b23221086c
Bashyitsi <p.shema1@alustudent.com>
2024-11-06 12:23:21 +0200

    maze kwiheba

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["localhost"],
+    domains: ["localhost,https://deutschconnectacademy.vercel.app/"],
     remotePatterns: [
       {
         protocol: "https",
