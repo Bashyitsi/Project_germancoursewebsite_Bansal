2e7d9206e262fc8003868c5a8aebbbf7956fbc2f
Bashyitsi <p.shema1@alustudent.com>
2024-11-06 11:49:47 +0200

    maze kwiheba

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
-import ToasterContext from "../context/ToastContext";
+import ToasterContext from "./context/ToastContext";
 
 export default function RootLayout({
   children,
