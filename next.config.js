d0dec3406c4ecdcad912d9c2e7a6404a5da236b0
Bashyitsi <p.shema1@alustudent.com>
2024-11-08 16:55:53 +0200

    adding new features

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost,https://deutschconnectacademy.vercel.app/"],
+    domains: ["deutschconnectacademy.vercel.app"],
     remotePatterns: [
       {
         protocol: "https",
